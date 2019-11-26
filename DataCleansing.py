e0de28099692a79a96fbaa732937a62021fcea52
jjcc <jjccforth@gmail.com>
2019-11-26 12:59:24 -0500

    update

--- DataCleansing.py
+++ DataCleansing.py
@@ -2,11 +2,32 @@ import pandas as pd
 
 datafile = "Data//updated_sharepoint.xlsm"
 
+FIELDS_SPECL_CHR = ['Collection Type','Data Steward Email']
+#Audience
 
-df = pd.read_excel(datafile)
+def remove_special_character(df,field,regex):
+    """
+    remove special characters from a column
+    :param df: data frame
+    :param field: field name. i.e. 'Collection Type'
+    :param regex: regex expression of the pattern that need to removed. i.e. r';#\d+'
+    :return: fixed data frame
+    """
+    dcolumn = df[field]
+    df[field] = dcolumn.str.replace(regex,'')
+    return df
 
-dse = df["Data Steward Email"]
-df["Data Steward Email"] = dse.str.replace(r';#\d+',"")
-df.to_excel("Data//new_updated_sp.xlsx")
 
-pass
+def main():
+    df = pd.read_excel(datafile)
+
+    for f in FIELDS_SPECL_CHR:
+        df = remove_special_character(df,f,r';#\d+')
+
+
+    df.to_excel("Data//new_updated_sp.xlsx")
+
+    pass
+
+if __name__ == '__main__':
+    main()
